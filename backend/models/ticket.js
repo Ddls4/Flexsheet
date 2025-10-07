4639344e752369736e10fe86b80d333ddbc66ced
gastongamerhz <ferreiragaston654@gmail.com>
2025-10-06 21:48:22 -0300

    agrega ticket

---
+++ backend/models/ticket.js
@@ -0,0 +1,15 @@
+// models/ticket.js
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema({
+  ID_T: Number,
+  Titulo: String,
+  Descripcion: String,
+  Estado: String,
+  ID_U: String,
+  ID_N: String,
+  Fecha_creacion: Date
+});
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+export default Ticket;
